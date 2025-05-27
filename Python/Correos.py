5156d4ec3a81b02843897644d97f4632fc34f740
paulo658 <paulopezgarcia5@gmail.com>
2025-05-27 20:03:28 +0200

    ACTUALIZAR EL PYTHON

--- Python/Correos.py
+++ Python/Correos.py
@@ -1,7 +1,23 @@
 import mysql.connector
 import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+def leer_credenciales(ruta):
+    credenciales = {}
+    with open(ruta, 'r') as f:
+        for linea in f:
+            if '=' in linea:
+                clave, valor = linea.strip().split('=', 1)
+                credenciales[clave.strip()] = valor.strip()
+    return credenciales
+
+ruta_script = os.path.dirname(__file__) 
+
+ruta_pass = os.path.join(ruta_script,'pass.inf')
+ruta_pass = os.path.normpath(ruta_script)
+data = leer_credenciales(ruta_pass)
 
 # Conexión a la base de datos MySQL
 conexion = mysql.connector.connect(
@@ -11,6 +27,7 @@ conexion = mysql.connector.connect(
     database="TU_BASE_DE_DATOS"
 )
 
+
 cursor = conexion.cursor(dictionary=True)
 cursor.execute("SELECT nombre, email FROM usuarios")  # Cambia 'usuarios' por tu tabla
 
@@ -19,10 +36,10 @@ with open('c:/Users/quebr/Desktop/PRACTICAS/Feria_vlc/supuesto1.html', 'r', enco
     html_template = f.read()
 
 # Configura tu servidor SMTP (ejemplo con Gmail)
-smtp_server = "smtp.gmail.com"
+smtp_server = "smtp.office365.com"
 smtp_port = 587
-smtp_user = "tu_email@gmail.com"
-smtp_pass = "tu_contraseña_o_app_password"
+smtp_user = data.get("mail")
+smtp_pass = data.get("pass")
 
 server = smtplib.SMTP(smtp_server, smtp_port)
 server.starttls()
